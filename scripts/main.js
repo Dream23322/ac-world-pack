586da34237ec5974bf075c36655e36dc1157645a
Dream23322 <you@example.com>
2024-09-09 11:37:18 +1000

    Add >clear and >anticheat

--- scripts/main.js
+++ scripts/main.js
@@ -9,32 +9,32 @@ world.beforeEvents.chatSend.subscribe((msg) => {
 
     if(message == ">spawn") {
         player.runCommandAsync(`tp @s ${config.commandData.spawn.x} ${config.commandData.spawn.y} ${config.commandData.spawn.z}`);
-        player.sendMessage(`§bAssistant §h> §aYou have been sent to spawn!`);
+        player.sendMessage(`§iAssistant §h> §aYou have been sent to spawn!`);
     } else if(message == ">fly") {
         if(player.hasTag("op")) {
             player.runCommandAsync(`ability @s mayfly true`);
-            player.sendMessage(`§bAssistant §h> §aFly enabled!`);
+            player.sendMessage(`§iAssistant §h> §aFly enabled!`);
         } else {
-            player.sendMessage(`§bAssistant §h> §cYou don't have permission to use this command! (MissingTag: "op")`);
+            player.sendMessage(`§iAssistant §h> §cYou don't have permission to use this command! (MissingTag: "op")`);
         }
     } else if(message == ">blocks") {
         player.runCommandAsync(`give @s ${config.commandData.blocks.blockid} 256`);
-        player.sendMessage(`§bAssistant §h> §aBlocks given!`);
+        player.sendMessage(`§iAssistant §h> §aBlocks given!`);
     } else if(message == ">speed") {
         player.runCommandAsync(config.commandData.speed.command);
-        player.sendMessage(`§bAssistant §h> §aYou have been given Speed 1 for 10000 seconds!`);
+        player.sendMessage(`§iAssistant §h> §aYou have been given Speed 1 for 10000 seconds!`);
     } else if(message == ">speed2") {
         player.runCommandAsync(config.commandData.speed2.command);
-        player.sendMessage(`§bAssistant §h> §aYou have been given Speed 2 for 10000 seconds!`);
+        player.sendMessage(`§iAssistant §h> §aYou have been given Speed 2 for 10000 seconds!`);
     } else if(message == ">jumpboost") {
         player.runCommandAsync(config.commandData.jumpboost.command);
-        player.sendMessage(`§bAssistant §h> §aYou have been given Jump Boost 1 for 10000 seconds!`);
+        player.sendMessage(`§iAssistant §h> §aYou have been given Jump Boost 1 for 10000 seconds!`);
     } else if(message == ">jumpboost2") {
         player.runCommandAsync(config.commandData.jumpboost2.command);
-        player.sendMessage(`§bAssistant §h> §aYou have been given Jump Boost 2 for 10000 seconds!`);
+        player.sendMessage(`§iAssistant §h> §aYou have been given Jump Boost 2 for 10000 seconds!`);
     } else if(message == ">sword") {
         player.runCommandAsync(config.commandData.sword.command);
-        player.sendMessage(`§bAssistant §h> §aSummoned sword!`);
+        player.sendMessage(`§iAssistant §h> §aSummoned sword!`);
     } else if(message == ">kit1") {
         try {
             // Kit 1:
@@ -62,9 +62,9 @@ world.beforeEvents.chatSend.subscribe((msg) => {
             // Set boots
             player.runCommandAsync(`give @s iron_boots`);
 
-            player.sendMessage(`§bAssistant §h> §aSummoned in Kit 1!`);
+            player.sendMessage(`§iAssistant §h> §aSummoned in Kit 1!`);
         } catch (e) {
-            player.sendMessage(`§bAssistant §h> §cError Summoning Kit 1: ${e.message}`);
+            player.sendMessage(`§iAssistant §h> §cError Summoning Kit 1: ${e.message}`);
         }
     } else if(message == ">kit2") {
         try {
@@ -82,25 +82,56 @@ world.beforeEvents.chatSend.subscribe((msg) => {
 
             player.runCommandAsync(`give @s fishing_rod 1`);
 
-            player.sendMessage(`§bAssistant §h> §aSummoned in OP Kit!`);
+            player.sendMessage(`§iAssistant §h> §aSummoned in OP Kit!`);
         } catch (e) {
-            player.sendMessage(`§bAssistant §h> §cError Summoning OP Kit: ${e.message}`);
+            player.sendMessage(`§iAssistant §h> §cError Summoning OP Kit: ${e.message}`);
         } 
     } else if(message == ">clear") {
         player.runCommandAsync(`clear @s`);
         player.runCommandAsync(`effect @s clear`);
-        player.sendMessage(`§bAssistant §h> §aCleared inventory and effects!`);
-    }
+        player.sendMessage(`§iAssistant §h> §aCleared inventory and effects!`);
+    } else if(message == ">help") {
+        //player.sendMessage(`§iAssistant §h> §aCommands: \n>spawn \n>fly \n>blocks \n>speed \n>speed2 \n>jumpboost \n>jumpboost2 \n>sword \n>kit1 \n>kit2`);
 
-    if(message == ">help") {
-        //player.sendMessage(`§bAssistant §h> §aCommands: \n>spawn \n>fly \n>blocks \n>speed \n>speed2 \n>jumpboost \n>jumpboost2 \n>sword \n>kit1 \n>kit2`);
-
-        player.sendMessage(`§h==========================================`);
+        player.sendMessage(`\n\n§h==========================================`);
         player.sendMessage("§l Commands")
         player.sendMessage(`§h------------------------------------------`);
         player.sendMessage(`>spawn - Teleports you to spawn\n>fly - Enables fly mode\n>blocks - Gives you 256 blocks\n>speed - Gives you speed 1 for 10000 seconds \n>speed2 - Gives you speed 2 for 10000 seconds \n>jumpboost - Gives you Jump Boost 1 For 10000 seconds \n>jumpboost2 - Gives you Jump Boost 2 for 10000 seconds\n>sword - Gives you a sword \n>kit1 - Summons in Kit 1 \n>kit2 - Summons in OP Kit\n>Clears inventory and effects`);
-        player.sendMessage(`§h==========================================`);
-    }   
+        player.sendMessage(`§h==========================================\n\n`);
+    } else if(message == ">anticheat") {
+        let can = false;
+        if(getScore(player, "gametestapi", 0) > 0) {
+            player.sendMessage("§iAssistant §h> §cAnticheat found: Scythe!");
+            can = true;
+        } else if(getScore(player, "tick_counter2", 0) > 0 && !can) {
+            player.sendMessage("§iAssistant §h> §cAnticheat found: Isolate!");
+            can = true;
+        } else if(player.hasTag("alive") && !can) {
+            player.sendMessage("§iAssistant §h> §cAnticheat found: Matrix!");
+            can = true;
+        }
+    }else {
+        if(message.charAt(0) == ">") {
+            player.sendMessage(`§iAssistant §h> §cInvalid command! Use >help for a list of commands!`);
+        }
+    }
+    if(message.charAt(0) == ">") {
+        msg.cancel = true;
+    }
+
 
 });
 
+
+
+export function getScore(player, objective, defaultValue = 0) {
+    if(typeof player !== "object") throw TypeError(`Error: player is type of ${typeof player}. Expected "object"`);
+    if(typeof objective !== "string") throw TypeError(`Error: objective is type of ${typeof objective}. Expected "string"`);
+    if(typeof defaultValue !== "number") throw TypeError(`Error: defaultValue is type of ${typeof defaultValue}. Expected "number"`);
+
+    try {
+       return world.scoreboard.getObjective(objective)?.getScore(player) ?? defaultValue;
+    } catch {
+        return defaultValue;
+    }
+}
